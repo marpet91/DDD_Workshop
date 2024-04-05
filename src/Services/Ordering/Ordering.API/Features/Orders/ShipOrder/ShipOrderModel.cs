e7116b0ebc1b39877454710eee58cd7b5d343a6e
Jimmy Bogard <jimmy.bogard@gmail.com>
2024-04-05 16:26:11 -0500

    Removing unneeded file

--- src/Services/Ordering/Ordering.API/Features/Orders/ShipOrder/ShipOrderModel.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace Microsoft.eShopOnContainers.Services.Ordering.API.Features.Orders.ShipOrder;
-
-public class ShipOrderRequest : IRequest<bool>
-{
-    public int OrderNumber { get; set; }
-}
